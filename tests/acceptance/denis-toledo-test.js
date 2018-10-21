29194174a4b36d0280a5c9a71495093cd1a170d2
Denis Toledo <contato@denistoledo.com.br>
2018-10-21 20:12:28 -0300

    creating routes and testing

--- tests/acceptance/denis-toledo-test.js
+++ tests/acceptance/denis-toledo-test.js
@@ -1,25 +1,50 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import {
+  visit,
+  currentURL,
+  click
+} from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
 module('Acceptance | denis toledo', function(hooks) {
   setupApplicationTest(hooks);
 
-  test('visiting /', async function(assert) {
+  test('should show portfolio as the home page', async function(assert) {
     await visit('/');
-
-    assert.equal(currentURL(), '/');
+    assert.equal(
+      currentURL(),
+      '/portfolio',
+      'should redirect automatically'
+    );
   });
 
-  test('should show portfolios as the home page', async function(assert) {
-
+  test('should link to portfolio page', async function(assert) {
+    await visit('/');
+    await click('.btn-portfolio');
+    assert.equal(
+      currentURL(),
+      '/portfolio',
+      'should navigate to portfolio'
+    );
   });
 
   test('should link to about page', async function(assert) {
-
+    await visit('/');
+    await click('.btn-about');
+    assert.equal(
+      currentURL(),
+      '/about',
+      'should navigate to about'
+    );
   });
 
   test('should link to contact page', async function(assert) {
-
+    await visit('/');
+    await click('.btn-contact');
+    assert.equal(
+      currentURL(),
+      '/contact',
+      'should navigate to contact'
+    );
   });
 });
